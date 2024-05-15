4bad61bb3d7cd898d3995120a3bbb2b4e98c7889
Jannisrfst <101525736+Jannisrfst@users.noreply.github.com>
2024-05-15 13:08:01 +0200

    Update backend.py
    Added debugging featurfes through python logging

--- backend.py
+++ backend.py
@@ -1,30 +1,36 @@
 import os
-import re  
-import mimetypes
+import logging
 from flask import Flask, render_template, jsonify, request, send_from_directory
+from urllib.parse import unquote
 from werkzeug.utils import secure_filename
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
 
 def list_subfolders(startpath):
+    """List all subfolders in the given directory path."""
     subfolders = [f for f in os.listdir(startpath) if os.path.isdir(os.path.join(startpath, f))]
     return subfolders
 
 def list_files(startpath):
+    """List all files in the given directory path."""
     files = [f for f in os.listdir(startpath) if os.path.isfile(os.path.join(startpath, f))]
     return files
 
 @app.route('/')
 def index():
+    """Render the main page with a list of subfolders from a predefined path."""
     root_path = r'C:\Users\JannisReufsteck\Documents'
     immediate_subfolders = list_subfolders(root_path)
     return render_template('index.html', immediate_subfolders=immediate_subfolders)
 
 @app.route('/subfolders', methods=['POST'])
 def get_subfolders():
+    """Return subfolders and files of a given directory path from POST request."""
     folder_path = request.json['path']
     base_path = r'C:\Users\JannisReufsteck\Documents'
-    folder_path = re.sub(r'\s+', '', folder_path)  # remove whitespaces
+    folder_path = unquote(folder_path)
     full_path = os.path.join(base_path, folder_path)
     subfolders = list_subfolders(full_path)
     files = list_files(full_path)
@@ -32,26 +38,22 @@ def get_subfolders():
 
 @app.route('/files/<path:filename>')
 def files(filename):
+    """Serve files from a specified path, handling various file types and errors."""
     base_path = r'C:\Users\JannisReufsteck\Documents'
-    filename = secure_filename(filename)
+    filename = unquote(filename)
     file_path = os.path.join(base_path, filename)
-    mime_type, _ = mimetypes.guess_type(file_path)
+
+    logging.info(f"Attempting to access file: {file_path}")
+    if not os.path.exists(file_path):
+        logging.error("File not found at the path.")
+        return jsonify({"error": "File not found"}), 404
 
     try:
-        with open(file_path, 'rb') as file:
-            file_content = file.read()
-            if 'text' in mime_type:
-                file_content = file_content.decode('utf-8')
-                return jsonify({'filename': filename, 'content': file_content, 'mime_type': mime_type})
-            elif 'image' in mime_type and 'image/svg+xml' != mime_type:
-                import base64
-                file_content = base64.b64encode(file_content).decode('utf-8')
-                return jsonify({'filename': filename, 'content': file_content, 'mime_type': mime_type})
-            else:
-                return send_from_directory(base_path, filename, as_attachment=True)
-    except FileNotFoundError:
-        return jsonify({"error": "File not found"}), 404
+        response = send_from_directory(directory=base_path, filename=filename, as_attachment=True)
+        logging.info("File sent successfully.")
+        return response
     except Exception as e:
+        logging.critical(f"An error occurred when trying to send the file: {e}")
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
